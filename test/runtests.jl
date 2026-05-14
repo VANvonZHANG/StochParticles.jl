ca98c264cc22ceb2186042180773ccf1fd769b2d
Fan Zhang <vanvonzhang@gmail.com>
2026-05-14 19:31:09 +0800

    chore(tests): wire I/O tests into full test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,6 +24,7 @@ end
     include("test_dilution.jl")
     include("test_integration.jl")
     include("test_diagnostics.jl")
+    include("test_io.jl")
     include("test_plotting.jl")
     include("test_brownian_precision.jl")
 end
