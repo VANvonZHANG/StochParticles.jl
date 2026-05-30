3c872fd1a23720cd307c0c77f213de7c8db66e7b
Fan ZHANG <vanvonzhang@gmail.com>
2026-05-30 14:39:44 +0800

    feat: add preset species library (AS, AN, BC, OA, H2O)
    Adds Species struct, preset aerosol species constants, and species_vectors() combiner for convenient simulation setup.

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,6 +17,7 @@ end
 @testset "StochParticles.jl" begin
     include("test_particle_system.jl")
     include("test_process_trait.jl")
+    include("test_preset_species.jl")
     include("test_condensation.jl")
     include("test_coagulation.jl")
     include("test_cnmc.jl")
